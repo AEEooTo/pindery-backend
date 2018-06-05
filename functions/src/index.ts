8a116fc1a68caf319bd75d33514142adbe04c55e
Edoardo Debenedetti <dedeswim@gmail.com>
2018-06-05 17:19:59 +0800

    Better format index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -8,26 +8,26 @@ admin.initializeApp(functions.config().firebase);
 const partiesReference: DocumentBuilder = functions.firestore.document('/cities/{city}/parties/{party}');
 
 export const newPartyNotification = partiesReference
-.onCreate(
-    (snap, context) => {
-        const city: string = context.params.city;
-        const capitalizedCity: string = capitalizeFirstLetter(city);
-        const newParty: DocumentData = snap.data();
-        const partyName: string = newParty.name;
-        console.log(partyName + ' in ' + city);
+    .onCreate(
+        (snap, context) => {
+            const city: string = context.params.city;
+            const capitalizedCity: string = capitalizeFirstLetter(city);
+            const newParty: DocumentData = snap.data();
+            const partyName: string = newParty.name;
+            console.log(partyName + ' in ' + city);
 
-        const message = {
-            notification: {
-                title: 'New party nearby!',
-                body: 'There\'s a new party in ' + capitalizedCity + ': ' + partyName + '. Join now!',
-            },
-            topic: city,
+            const message = {
+                notification: {
+                    title: 'New party nearby!',
+                    body: 'There\'s a new party in ' + capitalizedCity + ': ' + partyName + '. Join now!',
+                },
+                topic: city,
+            }
+
+            return admin.messaging().send(message);
         }
-
-        return admin.messaging().send(message);
-    }
-)
+    )
 
 function capitalizeFirstLetter(string: string) {
-return string.charAt(0).toUpperCase() + string.slice(1);
+    return string.charAt(0).toUpperCase() + string.slice(1);
 }
